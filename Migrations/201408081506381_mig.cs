d30c4034afdeabb9e726d173e841da9422b17c0d
KA95 <antonkl@mail.ru>
2014-08-08 18:10:37 +0300

    after

---
+++ Migrations/201408081506381_mig.cs
@@ -0,0 +1,294 @@
+namespace MyWebApp.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class mig : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Categories",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Comments",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Text = c.String(),
+                        UserId = c.String(maxLength: 128),
+                        ProblemId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.AspNetUsers", t => t.UserId)
+                .ForeignKey("dbo.Problems", t => t.ProblemId, cascadeDelete: true)
+                .Index(t => t.UserId)
+                .Index(t => t.ProblemId);
+            
+            CreateTable(
+                "dbo.Problems",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        Text = c.String(),
+                        CategoryId = c.Int(nullable: false),
+                        Author_Id = c.String(maxLength: 128),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.AspNetUsers", t => t.Author_Id)
+                .ForeignKey("dbo.Categories", t => t.CategoryId, cascadeDelete: true)
+                .Index(t => t.CategoryId)
+                .Index(t => t.Author_Id);
+            
+            CreateTable(
+                "dbo.AspNetUsers",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        Email = c.String(maxLength: 256),
+                        EmailConfirmed = c.Boolean(nullable: false),
+                        PasswordHash = c.String(),
+                        SecurityStamp = c.String(),
+                        PhoneNumber = c.String(),
+                        PhoneNumberConfirmed = c.Boolean(nullable: false),
+                        TwoFactorEnabled = c.Boolean(nullable: false),
+                        LockoutEndDateUtc = c.DateTime(),
+                        LockoutEnabled = c.Boolean(nullable: false),
+                        AccessFailedCount = c.Int(nullable: false),
+                        UserName = c.String(nullable: false, maxLength: 256),
+                    })
+                .PrimaryKey(t => t.Id)
+                .Index(t => t.UserName, unique: true, name: "UserNameIndex");
+            
+            CreateTable(
+                "dbo.UserAttemptedProblems",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        UserId = c.String(maxLength: 128),
+                        ProblemId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Problems", t => t.ProblemId, cascadeDelete: true)
+                .ForeignKey("dbo.AspNetUsers", t => t.UserId)
+                .Index(t => t.UserId)
+                .Index(t => t.ProblemId);
+            
+            CreateTable(
+                "dbo.AspNetUserClaims",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        UserId = c.String(nullable: false, maxLength: 128),
+                        ClaimType = c.String(),
+                        ClaimValue = c.String(),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.AspNetUsers", t => t.UserId, cascadeDelete: true)
+                .Index(t => t.UserId);
+            
+            CreateTable(
+                "dbo.Dislikes",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        UserId = c.String(maxLength: 128),
+                        ProblemId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Problems", t => t.ProblemId, cascadeDelete: true)
+                .ForeignKey("dbo.AspNetUsers", t => t.UserId)
+                .Index(t => t.UserId)
+                .Index(t => t.ProblemId);
+            
+            CreateTable(
+                "dbo.Images",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        URL = c.String(),
+                        UserId = c.String(maxLength: 128),
+                        ProblemId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Problems", t => t.ProblemId, cascadeDelete: true)
+                .ForeignKey("dbo.AspNetUsers", t => t.UserId)
+                .Index(t => t.UserId)
+                .Index(t => t.ProblemId);
+            
+            CreateTable(
+                "dbo.Likes",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        UserId = c.String(maxLength: 128),
+                        ProblemId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Problems", t => t.ProblemId, cascadeDelete: true)
+                .ForeignKey("dbo.AspNetUsers", t => t.UserId)
+                .Index(t => t.UserId)
+                .Index(t => t.ProblemId);
+            
+            CreateTable(
+                "dbo.AspNetUserLogins",
+                c => new
+                    {
+                        LoginProvider = c.String(nullable: false, maxLength: 128),
+                        ProviderKey = c.String(nullable: false, maxLength: 128),
+                        UserId = c.String(nullable: false, maxLength: 128),
+                    })
+                .PrimaryKey(t => new { t.LoginProvider, t.ProviderKey, t.UserId })
+                .ForeignKey("dbo.AspNetUsers", t => t.UserId, cascadeDelete: true)
+                .Index(t => t.UserId);
+            
+            CreateTable(
+                "dbo.AspNetUserRoles",
+                c => new
+                    {
+                        UserId = c.String(nullable: false, maxLength: 128),
+                        RoleId = c.String(nullable: false, maxLength: 128),
+                    })
+                .PrimaryKey(t => new { t.UserId, t.RoleId })
+                .ForeignKey("dbo.AspNetUsers", t => t.UserId, cascadeDelete: true)
+                .ForeignKey("dbo.AspNetRoles", t => t.RoleId, cascadeDelete: true)
+                .Index(t => t.UserId)
+                .Index(t => t.RoleId);
+            
+            CreateTable(
+                "dbo.UserSolvedProblems",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        UserId = c.String(maxLength: 128),
+                        ProblemId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Problems", t => t.ProblemId, cascadeDelete: true)
+                .ForeignKey("dbo.AspNetUsers", t => t.UserId)
+                .Index(t => t.UserId)
+                .Index(t => t.ProblemId);
+            
+            CreateTable(
+                "dbo.Videos",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        URL = c.String(),
+                        UserId = c.String(maxLength: 128),
+                        ProblemId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Problems", t => t.ProblemId, cascadeDelete: true)
+                .ForeignKey("dbo.AspNetUsers", t => t.UserId)
+                .Index(t => t.UserId)
+                .Index(t => t.ProblemId);
+            
+            CreateTable(
+                "dbo.Tags",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.AspNetRoles",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        Name = c.String(nullable: false, maxLength: 256),
+                    })
+                .PrimaryKey(t => t.Id)
+                .Index(t => t.Name, unique: true, name: "RoleNameIndex");
+            
+            CreateTable(
+                "dbo.TagProblems",
+                c => new
+                    {
+                        Tag_Id = c.Int(nullable: false),
+                        Problem_Id = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => new { t.Tag_Id, t.Problem_Id })
+                .ForeignKey("dbo.Tags", t => t.Tag_Id, cascadeDelete: true)
+                .ForeignKey("dbo.Problems", t => t.Problem_Id, cascadeDelete: true)
+                .Index(t => t.Tag_Id)
+                .Index(t => t.Problem_Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.AspNetUserRoles", "RoleId", "dbo.AspNetRoles");
+            DropForeignKey("dbo.Comments", "ProblemId", "dbo.Problems");
+            DropForeignKey("dbo.TagProblems", "Problem_Id", "dbo.Problems");
+            DropForeignKey("dbo.TagProblems", "Tag_Id", "dbo.Tags");
+            DropForeignKey("dbo.Problems", "CategoryId", "dbo.Categories");
+            DropForeignKey("dbo.Videos", "UserId", "dbo.AspNetUsers");
+            DropForeignKey("dbo.Videos", "ProblemId", "dbo.Problems");
+            DropForeignKey("dbo.UserSolvedProblems", "UserId", "dbo.AspNetUsers");
+            DropForeignKey("dbo.UserSolvedProblems", "ProblemId", "dbo.Problems");
+            DropForeignKey("dbo.AspNetUserRoles", "UserId", "dbo.AspNetUsers");
+            DropForeignKey("dbo.AspNetUserLogins", "UserId", "dbo.AspNetUsers");
+            DropForeignKey("dbo.Likes", "UserId", "dbo.AspNetUsers");
+            DropForeignKey("dbo.Likes", "ProblemId", "dbo.Problems");
+            DropForeignKey("dbo.Images", "UserId", "dbo.AspNetUsers");
+            DropForeignKey("dbo.Images", "ProblemId", "dbo.Problems");
+            DropForeignKey("dbo.Dislikes", "UserId", "dbo.AspNetUsers");
+            DropForeignKey("dbo.Dislikes", "ProblemId", "dbo.Problems");
+            DropForeignKey("dbo.Comments", "UserId", "dbo.AspNetUsers");
+            DropForeignKey("dbo.AspNetUserClaims", "UserId", "dbo.AspNetUsers");
+            DropForeignKey("dbo.Problems", "Author_Id", "dbo.AspNetUsers");
+            DropForeignKey("dbo.UserAttemptedProblems", "UserId", "dbo.AspNetUsers");
+            DropForeignKey("dbo.UserAttemptedProblems", "ProblemId", "dbo.Problems");
+            DropIndex("dbo.TagProblems", new[] { "Problem_Id" });
+            DropIndex("dbo.TagProblems", new[] { "Tag_Id" });
+            DropIndex("dbo.AspNetRoles", "RoleNameIndex");
+            DropIndex("dbo.Videos", new[] { "ProblemId" });
+            DropIndex("dbo.Videos", new[] { "UserId" });
+            DropIndex("dbo.UserSolvedProblems", new[] { "ProblemId" });
+            DropIndex("dbo.UserSolvedProblems", new[] { "UserId" });
+            DropIndex("dbo.AspNetUserRoles", new[] { "RoleId" });
+            DropIndex("dbo.AspNetUserRoles", new[] { "UserId" });
+            DropIndex("dbo.AspNetUserLogins", new[] { "UserId" });
+            DropIndex("dbo.Likes", new[] { "ProblemId" });
+            DropIndex("dbo.Likes", new[] { "UserId" });
+            DropIndex("dbo.Images", new[] { "ProblemId" });
+            DropIndex("dbo.Images", new[] { "UserId" });
+            DropIndex("dbo.Dislikes", new[] { "ProblemId" });
+            DropIndex("dbo.Dislikes", new[] { "UserId" });
+            DropIndex("dbo.AspNetUserClaims", new[] { "UserId" });
+            DropIndex("dbo.UserAttemptedProblems", new[] { "ProblemId" });
+            DropIndex("dbo.UserAttemptedProblems", new[] { "UserId" });
+            DropIndex("dbo.AspNetUsers", "UserNameIndex");
+            DropIndex("dbo.Problems", new[] { "Author_Id" });
+            DropIndex("dbo.Problems", new[] { "CategoryId" });
+            DropIndex("dbo.Comments", new[] { "ProblemId" });
+            DropIndex("dbo.Comments", new[] { "UserId" });
+            DropTable("dbo.TagProblems");
+            DropTable("dbo.AspNetRoles");
+            DropTable("dbo.Tags");
+            DropTable("dbo.Videos");
+            DropTable("dbo.UserSolvedProblems");
+            DropTable("dbo.AspNetUserRoles");
+            DropTable("dbo.AspNetUserLogins");
+            DropTable("dbo.Likes");
+            DropTable("dbo.Images");
+            DropTable("dbo.Dislikes");
+            DropTable("dbo.AspNetUserClaims");
+            DropTable("dbo.UserAttemptedProblems");
+            DropTable("dbo.AspNetUsers");
+            DropTable("dbo.Problems");
+            DropTable("dbo.Comments");
+            DropTable("dbo.Categories");
+        }
+    }
+}
