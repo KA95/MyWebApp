bbdac1374f2fa9782eba1e674de665b45b8abb53
KA95 <antonkl@mail.ru>
2014-08-19 13:48:05 +0300

    bug fixed

--- Models/IdentityModels.cs
+++ Models/IdentityModels.cs
@@ -31,7 +31,7 @@ namespace MyWebApp.Models
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
-        public DbSet<Answer> Dislikes { get; set; }
+        public DbSet<Dislike> Dislikes { get; set; }
         public DbSet<Category> Categories { get; set; }
         public DbSet<Comment> Comments { get; set; }
         public DbSet<Image> Images { get; set; }
