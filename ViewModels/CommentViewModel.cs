6ebfb67cc62c7da59c547d3687ee1027f96cca0a
KA95 <antonkl@mail.ru>
2014-08-19 22:27:00 +0300

    comments begins

---
+++ ViewModels/CommentViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MyWebApp.ViewModels
+{
+    public class CommentViewModel
+    {
+        [Required]
+        public string Text { get; set; }
+        public DateTime AddingTime { get; set; }
+        public string AuthorName { get; set; }
+        public int ProblemId { get; set; }
+    }
+}
